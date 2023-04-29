336a0057151636176f758b8501e345410b79bb54
DanielMelo404 <37639887+DanielMelo404@users.noreply.github.com>
2023-04-28 22:49:22 -0500

    hey

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+
+def home(request):
+    context = {}
+    return render(request,'core/home.html',context)
